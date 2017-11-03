0ebc1515de6d8605c809821d9c011b8224ae2ab3
Amanda Yang Ya Chun <amanda.yang93@titansoft.com.sg>
2017-11-03 18:07:23 +0800

    [technical] amanda upload

---
+++ kataPoker/kataPoker/KataPokerHanders.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace kataPoker
+{
+    class KataPokerHanders
+    {
+        public static string compare(string input)
+        {
+            string[] inputSplit = input.Split(' ');
+            List<string> Black =new List<string>();
+            List<string> White = new List<string>();
+
+            for (int i = 0; i < inputSplit.Length; i++)
+            {
+                if (i < 6)
+                {
+                    Black.Add(inputSplit[i]);
+                }
+                else
+                {
+                    White.Add(inputSplit[i]);
+                }
+            }
+
+            return "White wins. -With high card: Ace";
+        }
+    }
+}
---
+++ kataPoker/kataPoker/kataPoker.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace kataPoker
+{
+    [TestClass]
+    public class kataPoker
+    {
+        [TestMethod]
+        public void tie()
+        {
+            string expect = "White wins. -With high card: Ace";
+            Assert.AreEqual(expect, KataPokerHanders.compare("Black:2H 3D 5S 9C KD White: 2C 3H 4S 8C AH"));
+        }
+    }
+}
